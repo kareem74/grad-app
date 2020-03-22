81a448ea55eff89cf4d70b2acbb4857df146d1bf
basantahmed98 <ahmedbasant736@gmail.com>
2020-03-22 17:59:50 +0200

    setting

--- app/src/main/java/com/example/project/Register.java
+++ app/src/main/java/com/example/project/Register.java
@@ -27,6 +27,7 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Register.this,MainActivity.class);
+                startActivity(intent);
             }
         });
 
