27a64eedd24fbba29692b6522e14278218421198
basantahmed98 <ahmedbasant736@gmail.com>
2020-03-25 03:50:45 +0200

    settings

--- app/src/main/java/com/example/project/setting_page.java
+++ app/src/main/java/com/example/project/setting_page.java
@@ -23,7 +23,7 @@ extreme_txt=findViewById(R.id.extreme_value_txt);
 measuring_txt=findViewById(R.id.measure_txt);
 extreme_value_btn=findViewById(R.id.extreme_btn);
 measuring_period_btn=findViewById(R.id.measure_btn);
-
+minute_txt=findViewById(R.id.minute_txt);
 Notify_checbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -32,12 +32,14 @@ extreme_txt.setVisibility(View.VISIBLE);
 measuring_txt.setVisibility(View.VISIBLE);
 extreme_value_btn.setVisibility(View.VISIBLE);
 measuring_period_btn.setVisibility(View.VISIBLE);
+minute_txt.setVisibility(View.VISIBLE);
         }
         else{
             extreme_txt.setVisibility(View.INVISIBLE);
             measuring_txt.setVisibility(View.INVISIBLE);
             extreme_value_btn.setVisibility(View.INVISIBLE);
             measuring_period_btn.setVisibility(View.INVISIBLE);
+            minute_txt.setVisibility(View.INVISIBLE);
         }
     }
 });
