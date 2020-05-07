1c9f35e1e9550d01cf665fd597fc2f2c134a9f78
Kholoud <kholodmhmd7@gmail.com>
2020-05-07 21:48:14 +0200

    Merge branch 'master' of C:\Users\USER\grad-app with conflicts.

---
+++ app/src/main/java/com/example/project/Wind_API.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wind_API extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wind__a_p_i);
+    }
+}
