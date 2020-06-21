c63a43fc537e188fb15de120be72655cb8b92e6d
Kholoud <kholodmhmd7@gmail.com>
2020-06-21 23:42:47 +0200

    anim_splash

--- app/src/main/java/com/example/project/splash_screen.java
+++ app/src/main/java/com/example/project/splash_screen.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splash_screen extends AppCompatActivity {
+    ImageView icon ;
+    TextView smart_fish_txt;
+   Animation topAnim , BottomAnim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        icon = findViewById(R.id.icon_splash);
+        smart_fish_txt = findViewById(R.id.smart_aqua_txt);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        BottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+        icon.setAnimation(topAnim);
+        smart_fish_txt.setAnimation(BottomAnim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
