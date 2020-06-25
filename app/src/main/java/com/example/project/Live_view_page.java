48a8e5fba130aae331c92ee5581c31c3f672e917
Kareem74 <adelkareem902@gmail.com>
2020-06-25 02:02:18 +0200

    notification and sensors

--- app/src/main/java/com/example/project/Live_view_page.java
+++ app/src/main/java/com/example/project/Live_view_page.java
@@ -1,14 +1,131 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.ref.PhantomReference;
 
 public class Live_view_page extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    TextView PH_connection, TEMP_connection, NH3_connection;
+    Button show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_view_page);
+        PH_connection = findViewById(R.id.phe);
+        TEMP_connection = findViewById(R.id.tempe);
+        NH3_connection = findViewById(R.id.ammoniae);
+        show = findViewById(R.id.show);
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                databaseReference =FirebaseDatabase.getInstance().getReference().child("sensors");
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String PH = dataSnapshot.child("PH").getValue().toString();
+                        PH_connection.setText("ph value is " + PH);
+                        String TEMP = dataSnapshot.child("Tempreture").getValue().toString();
+                        TEMP_connection.setText("Temp value is " + TEMP);
+                        String NH3 = dataSnapshot.child("nh3").getValue().toString();
+                        TEMP_connection.setText("nh3 value is " + NH3);
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+        /*databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String phhh =PH_connection.getText().toString();
+                if (phhh.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"wait ",Toast.LENGTH_LONG).show();
+                }else {
+                    notification_PH();
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        }
+    private void notification_PH () {
+        String PHH = PH_connection.getText().toString();
+
+        String message = "the value of ph is ";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_HIGH);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
+                .setContentText("Ph value")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message + PHH);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(999, builder.build());
+
+    */}
     }
-}
+
+
