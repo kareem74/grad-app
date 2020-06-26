b7a02d00c5b2670976615422c64000a67730b301
Kholoud <kholodmhmd7@gmail.com>
2020-06-26 15:14:35 +0200

    notification_

--- app/src/main/java/com/example/project/Live_view_page.java
+++ app/src/main/java/com/example/project/Live_view_page.java
@@ -8,6 +8,9 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -22,114 +25,88 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.pusher.pushnotifications.PushNotifications;
 
 import java.lang.ref.PhantomReference;
 
-public class Live_view_page extends AppCompatActivity {
+public class Live_view_page extends AppCompatActivity  implements ValueEventListener{
 
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
+    TextView ph_read_txt , temp_read_txt , nh3_read_txt;
+    private  FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private  DatabaseReference ph_read = databaseReference.child("ph_status");
+    private DatabaseReference temp_read = databaseReference.child("temp_status");
 
-    EditText PH_connection,TEMP_connection,NH3_connection;
-    Button show;
-    int id=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_view_page);
-        PH_connection = (EditText) findViewById(R.id.phe);
-        TEMP_connection = (EditText) findViewById(R.id.tempe);
-
-        show = findViewById(R.id.show);
-
-        show.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                databaseReference =FirebaseDatabase.getInstance().getReference("sensors");
-                databaseReference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                            String PH = dataSnapshot.child("PH").getValue().toString();
-
-                            PH_connection.setText("the value of ph is :" + PH);
-                            String Temp = dataSnapshot.child("Temp").getValue().toString();
-                            TEMP_connection.setText("the value of temperutre is " + Temp);
-
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-            }
-        });/*
-
-        databaseReference.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                String phhh =dataSnapshot.child("PH").getValue().toString();
-
-                if (phhh.isEmpty()){
-                    Toast.makeText(getApplicationContext(),"wait ",Toast.LENGTH_LONG).show();
-                }else {
-                    notification_PH();
-                }
-
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        }
-    private void notification_PH () {
-        String PHH = dataSnapshot.child("PH").getValue().toString();
-
-        String message = "the value of ph is ";
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel =
-                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_HIGH);
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-        }
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
-                .setContentText("Ph value")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setAutoCancel(true)
-                .setContentText(message + PHH);
-
-        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
-        managerCompat.notify(999, builder.build());
-
-    */}
+        ph_read_txt = findViewById(R.id.ph__read_txt);
+        temp_read_txt = findViewById(R.id.temp_read_txt);
+        nh3_read_txt = findViewById(R.id.ammonia_read_txt);
     }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        if (snapshot.getValue(String.class) != null){
+            String key = snapshot.getKey();
+            if (key.equals("ph_status"))
+            {
+                String PH = snapshot.getValue(String.class);
+                ph_read_txt.setText(PH);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Live_view_page.this);
+                builder.setSmallIcon(R.drawable.notification);
+                builder.setContentTitle("New notification");
+                builder.setContentText("Current PH Value : "+PH)
+                        .setAutoCancel(true);
+                Intent ph_intent = new Intent(Live_view_page.this,setting_page.class);
+                ph_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                ph_intent.putExtra("PH value :",PH);
+                PendingIntent pendingIntent =   PendingIntent.getActivity(Live_view_page.this,0,ph_intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(0,builder.build());
+            }
+
+        }
+        if (snapshot.getValue(String.class) != null){
+            String key = snapshot.getKey();
+            if (key.equals("temp_status"))
+            {
+                String Temp = snapshot.getValue(String.class);
+                temp_read_txt.setText(Temp);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Live_view_page.this)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle("New notification")
+                .setContentText("Current Temp Value : "+ Temp)
+                        .setAutoCancel(true);
+                Intent Temp_intent = new Intent(Live_view_page.this,setting_page.class);
+                Temp_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Temp_intent.putExtra("Temp value :",Temp);
+                PendingIntent pendingIntent =   PendingIntent.getActivity(Live_view_page.this,0,Temp_intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(0,builder.build());
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+    @Override
+    protected  void  onStart()
+    {
+        super.onStart();
+        ph_read.addValueEventListener(this);
+        temp_read.addValueEventListener(this);
+
+    }
+}
 
 
