8f82738cc3666ce992c8add49b4585635adf4956
Kholoud <kholodmhmd7@gmail.com>
2020-07-20 19:50:11 +0200

    api

---
+++ app/src/main/java/com/example/project/API_page.java
@@ -0,0 +1,84 @@
+package com.example.project;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class API_page extends AppCompatActivity {
+    Button Wind_btn ;
+    TextView Wind_tv;
+    EditText city_et;
+    int humidity ,pressure;
+    String description;
+    double speed , degree;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Wind_btn = findViewById(R.id.wind_btn);
+        Wind_tv =findViewById(R.id.wind_view_tv);
+        city_et = findViewById(R.id.city_et);
+        Wind_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String strtxt =city_et.getText().toString();
+                connectToAPI(strtxt);
+                Wind_tv.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+    private  void  connectToAPI(String cityname)
+    {
+        String API_Link = "https://samples.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=439d4b804bc8187953eb36d2a8c26a02";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, API_Link,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject root = new JSONObject(response);
+                            JSONObject wind , main ,weather;
+                            wind = root.getJSONObject("wind");
+                            main = root.getJSONObject("main");
+                            weather = root.getJSONObject("weather");
+                             speed = wind.getDouble("speed");
+                            degree = wind.getDouble("deg");
+                            pressure = main.getInt("pressure");
+                            humidity = main.getInt("humidity");
+                            description = weather.getString("description");
+
+                            // String name;
+                            Log.i("weather","hey hello");
+                            //Toast.makeText(MainActivity.this,"Speed: "+speed+"\t"+"Degree: "+degree,Toast.LENGTH_LONG).show();
+                            Wind_tv.setText("The Speed of Wind : "+speed+"\t"+"The Degree Of Wind : "+degree +"\t"+"Pressure :" +pressure+"\t"+"Humidity :"+humidity+"\t"+"Description : "+description);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
+    }
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton live_btn, setting_btn , history_btn, Logout_btn;
+    ImageButton live_btn, setting_btn , weather_btn, Logout_btn;
     Animation Topanim , Bottomanim;
 
     @Override
@@ -24,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         live_btn = findViewById(R.id.live_icon);
         setting_btn = findViewById(R.id.setting_icon);
-        history_btn = findViewById(R.id.history_icon);
+        weather_btn = findViewById(R.id.weather_icon);
         Logout_btn = findViewById(R.id.logout_icon);
         Topanim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
         Bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
         live_btn.setAnimation(Topanim);
-        history_btn.setAnimation(Topanim);
+        weather_btn.setAnimation(Topanim);
         setting_btn.setAnimation(Bottomanim);
         Logout_btn.setAnimation(Bottomanim);
         live_btn.setOnClickListener(new View.OnClickListener() {
@@ -47,10 +47,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        history_btn.setOnClickListener(new View.OnClickListener() {
+        weather_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,history_page.class);
+                Intent intent = new Intent(MainActivity.this,API_page.class);
                 startActivity(intent);
             }
         });
