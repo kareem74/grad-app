b0ae8aced197a8437ec882f743b60b44b95a1b54
Kareem74 <adelkareem902@gmail.com>
2020-07-22 00:04:08 +0200

    setting

--- app/src/main/java/com/example/project/Live_view_page.java
+++ app/src/main/java/com/example/project/Live_view_page.java
@@ -38,7 +38,7 @@ public class Live_view_page extends AppCompatActivity  implements ValueEventList
     FloatingActionButton fabmain;
     Float TranslationY = 100f;
     boolean isMenuOpen = false;
-    double NH3_VALUE;
+    String NH3_VALUE;
     OvershootInterpolator interpolator;
 
     EditText PH_connection,TEMP_connection,NH3_connection;
@@ -73,6 +73,11 @@ public class Live_view_page extends AppCompatActivity  implements ValueEventList
                         PH_connection.setText(PH);
                          Temp = dataSnapshot.child("Temp").getValue().toString();
                         TEMP_connection.setText(Temp);
+                        double DPH = Double.parseDouble(PH);
+                        double DTEMP = Double.parseDouble(Temp);
+                        NH3_VALUE =(Temp+PH);
+
+                        NH3_connection.setText( NH3_VALUE);
 
                     }
                    // NH3_VALUE =Double.parseDouble(PH +Temp)*0.0007;
@@ -84,8 +89,8 @@ public class Live_view_page extends AppCompatActivity  implements ValueEventList
 
                     }
                 });
-                NH3_VALUE = (Double.parseDouble(PH + Temp)*0.0007);
-                NH3_connection.setText((int) NH3_VALUE);
+
+
             }
         });
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
