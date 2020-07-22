82f55bdfa17e909f6b495f9092e25e4402b3a8eb
Kholoud <kholodmhmd7@gmail.com>
2020-07-22 02:57:21 +0200

    help

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton live_btn, setting_btn , weather_btn, Logout_btn;
+    ImageButton live_btn, setting_btn , help_btn, Logout_btn;
     Animation Topanim , Bottomanim;
 
     @Override
@@ -24,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         live_btn = findViewById(R.id.live_icon);
         setting_btn = findViewById(R.id.setting_icon);
-        weather_btn = findViewById(R.id.weather_icon);
+        help_btn = findViewById(R.id.help_icon);
         Logout_btn = findViewById(R.id.logout_icon);
         Topanim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
         Bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
         live_btn.setAnimation(Topanim);
-        weather_btn.setAnimation(Topanim);
+        help_btn.setAnimation(Topanim);
         setting_btn.setAnimation(Bottomanim);
         Logout_btn.setAnimation(Bottomanim);
         live_btn.setOnClickListener(new View.OnClickListener() {
@@ -48,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    weather_btn.setOnClickListener(new View.OnClickListener() {
+    help_btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, API_page.class);
+            Intent intent = new Intent(MainActivity.this, slideshow.class);
             startActivity(intent);
         }
     });
